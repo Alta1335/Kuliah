84484d363ad8fe623fe6f0d60ae5f42d2d0ce15b
Tb.Alta <101176328+Alta1335@users.noreply.github.com>
2023-11-27 15:04:11 +0700

    JURNAL2(Modul1).py

--- GitHub Kuliah/JURNAL2(Modul1).py
+++ GitHub Kuliah/JURNAL2(Modul1).py
@@ -1,3 +1,5 @@
+# Tb. Alta Ulil Abshor
+
 """Seorang idol bernama Lucas asal Indonesia sering menggelar konser di Amerika
 Serikat. Lucas membutuhkan program untuk menghitung jumlah uangnya
 dalam dolar AS. Sebagai asistennya, bantu Lucas untuk membuat program
@@ -7,3 +9,5 @@ Asumsikan kurs Rupiah ke Dolar AS (1 Rupiah = 0.00007 Dolar AS)."""
 rupiah = float(input("Masukan jumlah uang dalam Rupiah: Rp."))
 dollar = rupiah * 0.00007
 print(f"Jumlah uang dalam Dollar AS adalah: ${dollar}")
+
+# Tb. Alta Ulil Abshor
